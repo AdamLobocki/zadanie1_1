62868aa3bd6af55199293ec736bceb2cef4010e0
Adam <adamlobocki@gmail.com>
2019-04-30 13:14:01 +0200

    Trzeci commit

--- src/Nauka.java
+++ src/Nauka.java
@@ -1,6 +1,7 @@
 public class Nauka {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
+        System.out.println("Kolejna zmiana");
     }
 
 }
