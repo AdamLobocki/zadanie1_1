0fd2736cf2b96ea29b03a6249e3470468e78792a
Adam <adamlobocki@gmail.com>
2019-04-30 13:18:02 +0200

    Piąty commit

--- src/Nauka.java
+++ src/Nauka.java
@@ -3,6 +3,7 @@ public class Nauka {
         System.out.println("Uczę się programowania i commitowania");
         System.out.println("Kolejna zmiana");
         System.out.println(2+2);
+        System.out.println("ostatnie aaaa");
     }
 
 }
