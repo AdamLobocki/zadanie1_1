97f7f97a6b1694c595fbc75dfdce54c71fa4af7d
Adam <adamlobocki@gmail.com>
2019-04-30 13:15:10 +0200

    Czwarty commit

--- src/Nauka.java
+++ src/Nauka.java
@@ -2,6 +2,7 @@ public class Nauka {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
         System.out.println("Kolejna zmiana");
+        System.out.println(2+2);
     }
 
 }
